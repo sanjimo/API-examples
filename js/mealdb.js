d11f92d7244a20cf67c9af0a289c2681276d7abb
Sanjida Islam <sanjimo123@gmail.com>
2021-08-29 00:54:40 +0600

    mealdb added

---
+++ js/mealdb.js
@@ -0,0 +1,50 @@
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchFood(data.meals))
+}
+
+const displaySearchFood = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail('${meal.idMeal}')" class="card border-warning">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+               <h5 class="card-title">${meal.strMeal}</h5>
+               <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>`;
+        searchResult.appendChild(div);
+    });
+}
+
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal => {
+   // console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img width="200px" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-warning">Go YOUTUBE!</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
+}
