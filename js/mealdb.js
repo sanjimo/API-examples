745ba548cb71c5f76356e7be4554eefbf839c060
Sanjida Islam <sanjimo123@gmail.com>
2021-08-29 19:06:58 +0600

    mealdb updated

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,29 +1,44 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    //clear data
     searchField.value = '';
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchFood(data.meals))
+    //if there is no seach item
+    if (searchText == '') {
+        window.alert('Type your searching food name please!');
+    }
+    //if there is any search item
+    else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchFood(data.meals))
+    }
 }
 
 const displaySearchFood = meals => {
     const searchResult = document.getElementById('search-result');
-    meals.forEach(meal => {
-        console.log(meal);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div onclick="loadMealDetail('${meal.idMeal}')" class="card border-warning">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-               <h5 class="card-title">${meal.strMeal}</h5>
-               <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-            </div>
-        </div>`;
-        searchResult.appendChild(div);
-    });
+    //clear data
+    searchResult.textContent = '';
+    if (meals.length == 0) {
+        //message dite hbe..pore add korbo..
+    }
+    else {
+        meals.forEach(meal => {
+            //console.log(meal);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div onclick="loadMealDetail('${meal.idMeal}')" class="card border-warning">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                   <h5 class="card-title">${meal.strMeal}</h5>
+                   <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                </div>
+            </div>`;
+            searchResult.appendChild(div);
+        });
+    }
 }
 
 const loadMealDetail = mealId => {
@@ -34,7 +49,7 @@ const loadMealDetail = mealId => {
 }
 
 const displayMealDetail = meal => {
-   // console.log(meal);
+    // console.log(meal);
     const mealDetails = document.getElementById('meal-details');
     const div = document.createElement('div');
     div.classList.add('card');
